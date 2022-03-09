9fe0869bd1872de629b16039739cb0fb3b116170
Gobl <101265736+Goble450@users.noreply.github.com>
2022-03-09 13:42:57 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,104 @@
+input.onPinPressed(TouchPin.P0, function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . # . # .
+        . # . # .
+        . . # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        # . . . #
+        # # . . #
+        # . # . #
+        # . . # #
+        # . . . #
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # . .
+        . # . # .
+        . # . # .
+        . # . # .
+        . # # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        . # . . .
+        . # # # .
+        . # . . .
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # . .
+        . # . # .
+        . # # . .
+        . # . # .
+        . # . # .
+        `)
+    basic.pause(1000)
+    basic.showLeds(`
+        . # # . .
+        . # . # .
+        . # # . .
+        . # . # .
+        . # . # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        . # . . .
+        . # # # .
+        . # . . .
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # . .
+        . # . # .
+        . # # . .
+        . # . . .
+        . # . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # . .
+        . # . # .
+        . # # . .
+        . # . # .
+        . # . # .
+        `)
+})
+basic.showLeds(`
+    . # . # .
+    . # . # .
+    . # # # .
+    . # . # .
+    . # . # .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    . # # # .
+    . . # . .
+    . . # . .
+    . . # . .
+    . # # # .
+    `)
