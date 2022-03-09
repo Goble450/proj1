042d11122659ff4e35ff8f3eb91d27cbb24d9c88
Gobl <101265736+Goble450@users.noreply.github.com>
2022-03-09 13:52:50 +0000

    Added new functions

--- main.ts
+++ main.ts
@@ -38,54 +38,100 @@ input.onPinPressed(TouchPin.P0, function () {
         . # . # .
         . # . # .
         `)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+    basic.showLeds(`
+        . # # . .
+        . # . # .
+        . # # . .
+        . # . # .
+        . # . # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        . # . . .
+        . # # # .
+        . # . . .
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # . .
+        . # . # .
+        . # # . .
+        . # . . .
+        . # . . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        . # # # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # . .
+        . # . # .
+        . # # . .
+        . # . # .
+        . # . # .
+        `)
+})
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("B A G A G F A C5 ", 120)
+})
+input.onPinPressed(TouchPin.P1, function () {
+    basic.showLeds(`
+        . . # # .
+        . # . # .
+        . . # # .
+        . . . # #
+        . . . # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . # . # .
+        . # . # .
+        . . # . .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # # # .
+        . # . # .
+        . # . # .
+        `)
+    basic.pause(100)
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . # # . .
+        . # . # .
+        . # . # .
+        `)
     basic.pause(1000)
-    basic.showLeds(`
-        . # # . .
-        . # . # .
-        . # # . .
-        . # . # .
-        . # . # .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . # # # .
-        . # . . .
-        . # # # .
-        . # . . .
-        . # # # .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . # # . .
-        . # . # .
-        . # # . .
-        . # . . .
-        . # . . .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . # # # .
-        . # . # .
-        . # # # .
-        . # . # .
-        . # . # .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . # # # .
-        . . # . .
-        . . # . .
-        . . # . .
-        . # # # .
-        `)
-    basic.pause(100)
-    basic.showLeds(`
-        . # # . .
-        . # . # .
-        . # # . .
-        . # . # .
-        . # . # .
-        `)
+    basic.showIcon(IconNames.Duck)
 })
 basic.showLeds(`
     . # . # .
